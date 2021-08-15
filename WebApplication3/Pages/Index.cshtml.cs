ada1c7c3a074a6215c6aedc963ad2c24bd9ddbfc
João Pedro <joaopedrosantos@gmail.com>
2021-08-15 19:16:26 +0100

    commit files
    tentativa 5

---
+++ WebApplication3/Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Pages.Models;
+
+namespace WebApplication3.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+        public int Width { get; set; }
+        [BindProperty]
+        public int Ratio { get; set; }
+        [BindProperty]
+        public int Rim { get; set; }
+        
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public WheelModel Wheel { get; set; }  //cria uma instancia 'Wheel' do tipo WheelModel
+        
+        public void OnGet()
+        {
+           
+        }
+
+        
+
+        public void OnPost()
+        {
+            
+        }
+    }
+}
